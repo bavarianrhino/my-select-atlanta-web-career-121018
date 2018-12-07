32eb14a8ed6fdab62fed7f172a864fae293d4078
Ryan Riesenberger <rjriesenberger@gmail.com>
2018-12-07 08:17:17 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,27 @@
 def my_select(collection)
- # your code here!
+  if block_given?
+    i = 0
+    
+    return_collection = []
+    while i < collection.length
+      yield if collection[i] === true
+              return_collection.push(collection[i])
+      i = i + 1 
+    end
+  end
+  return_collection
 end
+
+
+# def my_select(collection)
+#   if block_given?
+#     i = 0
+    
+#     selected_arr_items = []
+#     while i < collection.length
+#       selected_arr_items.push (yield(collection[i] == true))
+#       i = i + 1 
+#     end
+#   end
+#   return selected_arr_items
+# end
